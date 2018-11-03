3c06af367f9975fcd7418b12c1530b1c72f986ed
Corbeau01 <corvus-corax@hotmail.com>
2018-11-03 15:00:21 -0400

    enemyScript

---
+++ Assets/DetectionEnemy.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectionEnemy : MonoBehaviour
+{
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject)
+        {
+            //Trigger
+            //
+        }
+    }
+    void MoveToward()
+    {
+
+    }
+    void Start ()
+    {
+		
+	}
+	
+	
+	void Update ()
+    {
+		
+	}
+}
