23f2ba9a774b24b4ae1d9d4b22767a402cbe35ef
Corbeau01 <corvus-corax@hotmail.com>
2018-11-03 09:11:58 -0400

    Je vois envois le script
    Creez un empty game object Generator et mettez mon script dessu

---
+++ Assets/Scipts/TileInfo.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileInfo : MonoBehaviour
+{
+    public int PositionX;
+    public int PositionY;
+    public int type;
+    public int Lumiere;
+    public int Etage;
+    public GameObject myGo;
+    public TileInfo(int x, int y,Color P,int etage)
+    {
+        type = (int)P.r;
+        Lumiere = (int)P.g;
+        Etage = etage;
+        PositionX = x;
+        PositionY = y;
+
+    }
+	
+}
