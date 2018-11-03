0b2e8cbffdd964ac4a38573a09ff43c2dde0531f
Corbeau01 <corvus-corax@hotmail.com>
2018-11-03 14:21:53 -0400

    FuckitBockit

--- Assets/Scipts/Generator.cs
+++ Assets/Scipts/Generator.cs
@@ -11,9 +11,9 @@ public class Generator : MonoBehaviour
     public GameObject TilePrefab;//Le prefab des thuiles generes
     public Transform MapHolder;
     Dictionary<TileInfo, GameObject> mapTItoGO = new Dictionary<TileInfo, GameObject>();//map les info vers le visuel de la thuil
-    Dictionary<GameObject, TileInfo> mapGOtoTI = new Dictionary<GameObject, TileInfo>();//map le visuel vers les info
-    public Dictionary<int[], TileInfo> mapCootoTI = new Dictionary<int[], TileInfo>();//map les coo vers les tileinfo
-    List<TileInfo> mapinfo = new List<TileInfo>();//Contiens toutes les info de la map
+    public Dictionary<GameObject, TileInfo> mapGOtoTI = new Dictionary<GameObject, TileInfo>();//map le visuel vers les info
+    public Dictionary<string, TileInfo> mapCootoTI = new Dictionary<string, TileInfo>();//map les coo vers les tileinfo
+    public List<TileInfo> mapinfo = new List<TileInfo>();//Contiens toutes les info de la map
     void GenerateMap1()
     {
 
@@ -25,20 +25,31 @@ public class Generator : MonoBehaviour
                 TileInfo TI = new TileInfo(x,y,P,0);//Tile info
                 GameObject TIGO = Instantiate(TilePrefab, new Vector3(x, 0, y), Quaternion.identity,MapHolder);//Tile PRefab
                 Mapping(TIGO, TI,x,y);//Mapping des deux dans mes dictionaires
-
+                
                 UpdateGo(TI);//Update le tile prefab attribue le ti au go et le go au ti
+                StartLocation(x, y, TI);
+                UpdateGo(TI);
             }
         }
     }
-
+    void StartLocation(int x,int y,TileInfo ti)
+    {
+        if(x==2&&y==2)
+        {
+            mapTItoGO[ti].GetComponent<TileUpdater>().StartLocation = true; 
+        }
+    }
+    public string CootoString(int x,int y)
+    {
+        return x.ToString() +","+ y.ToString();
+    }
     void Mapping(GameObject go, TileInfo ti,int x,int y)
     {
         mapGOtoTI.Add(go, ti);
         mapTItoGO.Add(ti, go);
         mapinfo.Add(ti);
-        int[] coo = new int[2];
-        coo[0] = x;
-        coo[1] = y;
+        string coo = CootoString(x, y);
+
         mapCootoTI.Add(coo, ti);
 
     }
@@ -50,4 +61,5 @@ public class Generator : MonoBehaviour
     {
         GenerateMap1();
     }
+
 }
