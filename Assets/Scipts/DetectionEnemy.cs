f31f823f247e2cbd8d35a6b2eb0a2ba4207677e0
Corbeau01 <corvus-corax@hotmail.com>
2018-11-03 15:16:46 -0400

    go ninja go

---
+++ Assets/Scipts/DetectionEnemy.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectionEnemy : MonoBehaviour
+{
+    public PlayerController Player;
+    GameObject PlayerGo;
+    bool Triggerd=false;
+    private void OnTriggerEnter(Collider other)    
+    {
+        if(other.gameObject==PlayerGo)
+        {
+
+            Triggerd = true;
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject == PlayerGo)
+        {
+
+            Triggerd = false;
+        }
+    }
+    void MoveToward()
+    {
+
+        Transform target = PlayerGo.transform;
+        Vector3 MyPosition = this.transform.parent.transform.position;
+        Vector3.MoveTowards(MyPosition, target.position, 0.2f);
+    }
+    void Start ()
+    {
+        Player = FindObjectOfType<PlayerController>();
+        PlayerGo = Player.gameObject;
+	}
+	
+	
+	void Update ()
+    {
+        
+		if(Triggerd)
+        {
+            MoveToward();
+        }
+	}
+}
