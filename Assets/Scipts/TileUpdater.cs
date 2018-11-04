b7dc2e514a47e58467ca0eeb51f26e1c52515fd4
Corbeau01 <corvus-corax@hotmail.com>
2018-11-03 21:32:26 -0400

    Je Vous Aimes

--- Assets/Scipts/TileUpdater.cs
+++ Assets/Scipts/TileUpdater.cs
@@ -12,10 +12,10 @@ public class TileUpdater : MonoBehaviour
     PlayerController player;
     public bool StartLocation = false;
     Generator GM;
-    public Material PasDansLaNeigeMat;
+    //public Material PasDansLaNeigeMat;
     public bool HasPasDansLaNeige;
-    public int PasDansLaNeigeTimer=5;
-    GameObject PasDansLaNeige;
+    
+    public GameObject SnowStep;
     public int DirectionDePasDansLaNeige = 0;
     void Start()
     {
@@ -45,16 +45,8 @@ public class TileUpdater : MonoBehaviour
         
     }
     
-    public void UpdateMeCurrent()
-    {
-        if (HasPasDansLaNeige) 
-        {
-
-            Instantiate(PasDansLaNeige, new Vector3(myInfo.PositionX, 0.2f, myInfo.PositionY), Quaternion.identity);
-
-
-        }
-    }
+    
+    
     public void UpdateMe(TileInfo ti)
     {
         this.transform.position = new Vector3(ti.PositionX, 0, ti.PositionY);
---
+++ Assets/SnowStepFade.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnowStepFade : MonoBehaviour
+{
+    public int PasDansLaNeigeTimer = 5;
+    public void PasDansLaNeigeTimerDown()
+    {
+
+        PasDansLaNeigeTimer--;
+        Color NewColor = new Color(1, 1, 1, (float)(PasDansLaNeigeTimer / 5f));
+        //GetComponentInChildren<SpriteRenderer>().color = NewColor;
+        GetComponentInChildren<SpriteRenderer>().material.color = NewColor;
+
+        if (PasDansLaNeigeTimer<=0)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+}
