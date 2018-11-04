10015883508cbd97e9f01ec2d52b59ea96eed9f2
ppouch <alexandre.lessard.2@ens.etsmtl.ca>
2018-11-04 16:39:07 -0500

    wolf fix

--- Assets/LowerImage.cs
+++ Assets/LowerImage.cs
@@ -4,14 +4,15 @@ using UnityEngine;
 
 public class LowerImage : MonoBehaviour {
 
+	Vector3 startPosition;
 	// Use this for initialization
 	void Start () {
-		
+		startPosition = transform.position;
 	}
 	
 	// Update is called once per frame
 	void Update ()
 	{
-		transform.position = new Vector3(0, 0, 0);
+		transform.position = startPosition - new Vector3(0, 10, 0);
 	}
 }
